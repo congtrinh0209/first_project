940c566886066c3ee721318e542b6b9bec354655
congtrinh0209 <congtrinh0209@gmail.com>
2017-08-24 00:01:43 +0700

    aside componant

--- app/js/home.js
+++ app/js/home.js
@@ -1,107 +1,79 @@
-'use strict';
+"use strict";
 
 //**Scroll fixed menu navigation-mobile,tablet
 $(document).ready(function () {
     var window_width = innerWidth;
     if (window_width <= 768) {
-        $('.header').attr('data-offset-top', '89');
-    };
+        $(".header").attr("data-offset-top", "89");
+    }
     //*****Hiển thị sign-in, sign-up
-    if ($(location).attr('href').search('psw') > 0) {
-        $('.navbar-right').css('display', 'none');
-        $('.info-acc').css('display', 'block');
+    if ($(location).attr("href").search("psw") > 0) {
+        $(".navbar-right").css("display", "none");
+        $("info-acc").css("display", "block");
     }
     $(".sign-out").on("click", function () {
-        window.location.href = 'home.html';
+        window.location.href = "home.html";
     });
-});
-//*************Check sign-in//demo 2 user_data
-var users = [{
-    fullname: 'Trịnh Công Trình',
-    mobile: '0868979193',
-    email: 'congtrinh0209@gmail.com',
-    pass: '02092008'
-}, {
-    fullname: 'Vũ Thị Quý',
-    mobile: '0972047443',
-    email: 'vuquy@gmail.com',
-    pass: '123456789'
-}];
-$('#sign-in').on('submit', function () {
-    var error = true;
-    var _iteratorNormalCompletion = true;
-    var _didIteratorError = false;
-    var _iteratorError = undefined;
-
-    try {
-        for (var _iterator = users[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-            var user = _step.value;
-
-            if ($('#uname').val() != user.mobile && $('#uname').val() != user.email || $('#psw').val() != user.pass) {
-                $('#psw').next('span').html('<i class="fa fa-exclamation-circle"></i>' + 'Tên đăng nhập hoặc mật khẩu không chính xác</br>');
-                error = false;
-            } else {
-                if ($('#uname').val() == user.mobile || $('#uname').val() == user.email && $('#psw').val() == user.pass) {
-                    $('#uname').next('span').text('');
-                    $('#psw').next('span').text('');
-                    $('.error3').css('padding', '0px');
-                    $('.error1').css('padding', '0px');
-                    return true;
-                }
-            }
-        }
-    } catch (err) {
-        _didIteratorError = true;
-        _iteratorError = err;
-    } finally {
-        try {
-            if (!_iteratorNormalCompletion && _iterator.return) {
-                _iterator.return();
-            }
-        } finally {
-            if (_didIteratorError) {
-                throw _iteratorError;
-            }
-        }
-    }
-
-    return error;
-});
-//****************Request send password
-$('#request-pass').on('submit', function () {
-    var error = true;
-    var _iteratorNormalCompletion2 = true;
-    var _didIteratorError2 = false;
-    var _iteratorError2 = undefined;
-
-    try {
-        for (var _iterator2 = users[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-            var user = _step2.value;
-
-            if ($('#send-mobile').val() != user.mobile) {
-                $('#send-mobile').next('span').html('<i class="fa fa-exclamation-circle"></i>' + ' Số điện thoại không tồn tại trên hệ thống!</br>');
-                error = false;
-            } else {
-                $('#send-mobile').next('span').text('');
-                $('.error3').css('padding', '0px');
-                alert('Vui lòng kiểm tra tin nhắn để nhận lại mật khẩu!');
+    //*************Check login//demo 2 user_data
+    //****Lấy dữ liệu từ JSON - Gán cho biến
+    var data_json;
+    $.getJSON("./data.json", function (data) {
+        data_json = data;
+        return data_json;
+    });
+    //***Check login
+    $(".login").on("click", function () {
+        $.each(data_json.users, function (key, value) {
+            if ($("#uname").val() == value.mobile && $("#psw").val() == value.pass || $("#uname").val() == value.email && $("#psw").val() == value.pass) {
+                $("#uname").next("span").text("");
+                $("#psw").next("span").text("");
+                $(".error3").css("padding", "0px");
+                $(".error1").css("padding", "0px");
+                $("#sign-in").submit();
                 return true;
+            } else {
+                $("#psw").next("span").html('<i class="fa fa-exclamation-circle"></i>' + 'Tên đăng nhập hoặc mật khẩu không chính xác</br>');
             }
+        });
+        return false;
+    });
+    //****************Request send password
+    $("#send").on("click", function () {
+        $.each(data_json.users, function (key, value) {
+            if ($("#send-mobile").val() == value.mobile) {
+                $("#send-mobile").next("span").text("");
+                $(".error3").css("padding", "0px");
+                alert("Vui lòng kiểm tra tin nhắn để nhận lại mật khẩu!");
+                $("#request-pass").submit();
+                return true;
+            } else {
+                $("#send-mobile").next("span").html('<i class="fa fa-exclamation-circle"></i>' + ' Số điện thoại không tồn tại trên hệ thống!</br>');
+            }
+        });
+        return false;
+    });
+    //*************Load các quận huyện theo địa danh-menu search
+    function load_locale() {
+        var display_locale = "<option>Quận, Huyện</option>";
+        if ($(".locale-item").val() == "Hà Nội") {
+            $.each(data_json.district_locale.district_hn, function (key, value) {
+                display_locale += "<option>" + value + "</option>";
+            });
+        } else if ($(".locale-item").val() == "Đà Nẵng") {
+            $.each(data_json.district_locale.district_dn, function (key, value) {
+                display_locale += "<option>" + value + "</option>";
+            });
+        } else if ($(".locale-item").val() == "TP.Hồ Chí Minh") {
+            $.each(data_json.district_locale.district_hcm, function (key, value) {
+                display_locale += "<option>" + value + "</option>";
+            });
+        } else {
+            display_locale += "<option>Quận, Huyện</option>";
         }
-    } catch (err) {
-        _didIteratorError2 = true;
-        _iteratorError2 = err;
-    } finally {
-        try {
-            if (!_iteratorNormalCompletion2 && _iterator2.return) {
-                _iterator2.return();
-            }
-        } finally {
-            if (_didIteratorError2) {
-                throw _iteratorError2;
-            }
-        }
+        $(".locale_district").html(display_locale);
     }
-
-    return error;
+    load_locale();
+    $(".locale-item").click(function () {
+        load_locale();
+    });
 });
--- app/jses6/home.js
+++ app/jses6/home.js
@@ -2,65 +2,85 @@
 $(document).ready(function() {
     var window_width = innerWidth;
     if (window_width <= 768) {
-        $('.header').attr('data-offset-top','89')
-    };
+        $(".header").attr("data-offset-top", "89");
+    }
     //*****Hiển thị sign-in, sign-up
-    if ($(location).attr('href').search('psw')>0){
-        $('.navbar-right').css('display','none');
-        $('.info-acc').css('display','block');
+    if ($(location).attr("href").search("psw")>0){
+        $(".navbar-right").css("display", "none");
+        $("info-acc").css("display", "block");
     }
     $(".sign-out").on("click",()=>{
-        window.location.href='home.html';
-})
-});
-//*************Check sign-in//demo 2 user_data
-var users = [
-    {
-        fullname: 'Trịnh Công Trình',
-        mobile: '0868979193',
-        email: 'congtrinh0209@gmail.com',
-        pass: '02092008'
-    },
-    {
-        fullname: 'Vũ Thị Quý',
-        mobile: '0972047443',
-        email: 'vuquy@gmail.com',
-        pass: '123456789'
-    }
-];
-$('#sign-in').on('submit',()=>{
-    var error = true;
-    for (let user of users){
-        if ($('#uname').val() != user.mobile && $('#uname').val() != user.email || $('#psw').val() != user.pass) {
-            $('#psw').next('span').html('<i class="fa fa-exclamation-circle"></i>' + 'Tên đăng nhập hoặc mật khẩu không chính xác</br>');
-            error = false;
-        } else {
-            if($('#uname').val() == user.mobile || $('#uname').val() == user.email && $('#psw').val() == user.pass){
-                $('#uname').next('span').text('');
-                $('#psw').next('span').text('');
-                $('.error3').css('padding', '0px');
-                $('.error1').css('padding', '0px');
+        window.location.href = "home.html";
+    })
+    //*************Check login//demo 2 user_data
+        //****Lấy dữ liệu từ JSON - Gán cho biến
+    var data_json;
+    $.getJSON("./data.json", (data)=>{
+        data_json = data;
+        return data_json;
+    });
+        //***Check login
+    $(".login").on("click", ()=>{
+        $.each(data_json.users,(key, value)=>{
+            if($("#uname").val() == value.mobile&& $("#psw").val() == value.pass || $("#uname").val() == value.email && $("#psw").val() == value.pass){
+                $("#uname").next("span").text("");
+                $("#psw").next("span").text("");
+                $(".error3").css("padding", "0px");
+                $(".error1").css("padding", "0px");
+                $("#sign-in").submit();
                 return true;
-            }
-        }
-    }
-    return error;
-});
-//****************Request send password
-$('#request-pass').on('submit',()=> {
-    var error = true;
-        for (let user of users) {
-            if ($('#send-mobile').val() != user.mobile) {
-                $('#send-mobile').next('span').html('<i class="fa fa-exclamation-circle"></i>' + ' Số điện thoại không tồn tại trên hệ thống!</br>');
-                error = false;
             }
             else {
-                $('#send-mobile').next('span').text('');
-                $('.error3').css('padding', '0px');
-                alert('Vui lòng kiểm tra tin nhắn để nhận lại mật khẩu!');
+                $("#psw").next("span").html('<i class="fa fa-exclamation-circle"></i>' + 'Tên đăng nhập hoặc mật khẩu không chính xác</br>');
+            }
+        });
+        return false;
+    });
+        //****************Request send password
+    $("#send").on("click",()=> {
+        $.each(data_json.users,(key, value)=>{
+            if ($("#send-mobile").val() == value.mobile) {
+                $("#send-mobile").next("span").text("");
+                $(".error3").css("padding", "0px");
+                alert("Vui lòng kiểm tra tin nhắn để nhận lại mật khẩu!");
+                $("#request-pass").submit();
                 return true;
             }
+            else {
+                $("#send-mobile").next("span").html('<i class="fa fa-exclamation-circle"></i>' + ' Số điện thoại không tồn tại trên hệ thống!</br>');
+            }
+        });
+        return false;
+    });
+    //*************Load các quận huyện theo địa danh-menu search
+    function load_locale(){
+        var display_locale = "<option>Quận, Huyện</option>";
+        if ($(".locale-item").val() == "Hà Nội"){
+            $.each(data_json.district_locale.district_hn,(key,value)=> {
+                display_locale += "<option>"+value+"</option>";
+        });
         }
-    return error;
-})
+        else if ($(".locale-item").val() == "Đà Nẵng"){
+            $.each(data_json.district_locale.district_dn,(key,value)=> {
+                display_locale += "<option>"+value+"</option>";
+        });
+        }
+        else if ($(".locale-item").val() == "TP.Hồ Chí Minh"){
+            $.each(data_json.district_locale.district_hcm,(key,value)=> {
+                display_locale += "<option>"+value+"</option>";
+        });
+        }
+        else {
+            display_locale += "<option>Quận, Huyện</option>";
+        }
+        $(".locale_district").html(display_locale);
+    }
+    load_locale();
+    $(".locale-item").click(function() {
+        load_locale();
+    });
+
+
+
+});
 
