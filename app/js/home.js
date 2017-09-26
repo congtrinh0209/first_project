0f14817bc7bca2a1f7c390e785e2a74ea5275fe0
congtrinh0209 <congtrinh0209@gmail.com>
2017-09-27 00:25:17 +0700

    post.html, aout-us.html, rules.html

--- app/js/home.js
+++ app/js/home.js
@@ -1,6 +1,19 @@
-"use strict";
+'use strict';
 
-//**Scroll fixed menu navigation-mobile,tablet
+/********** Load ảnh đại diện trang Manager  **********/
+function previewFile() {
+    var preview = document.getElementById('displayImg');
+    var file = document.querySelector('input[type=file]').files[0];
+    var reader = new FileReader();
+
+    reader.addEventListener("load", function () {
+        preview.src = reader.result;
+    }, false);
+
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+}
 $(document).ready(function () {
     /***Gán dữ liệu vào menu search khi chọn loại hình tim kiem******/
     $(".room").on('click', function () {
@@ -53,7 +66,7 @@ $(document).ready(function () {
                 $("#sign-in").submit();
                 return true;
             } else {
-                $("#psw").next("span").html("<i class=\"fa fa-exclamation-circle\"></i>T\xEAn \u0111\u0103ng nh\u1EADp ho\u1EB7c m\u1EADt kh\u1EA9u kh\xF4ng ch\xEDnh x\xE1c</br>");
+                $("#psw").next("span").html('<i class="fa fa-exclamation-circle"></i>T\xEAn \u0111\u0103ng nh\u1EADp ho\u1EB7c m\u1EADt kh\u1EA9u kh\xF4ng ch\xEDnh x\xE1c</br>');
             }
         });
         return false;
@@ -68,7 +81,7 @@ $(document).ready(function () {
                 $("#request-pass").submit();
                 return true;
             } else {
-                $("#send-mobile").next("span").html("<i class=\"fa fa-exclamation-circle\"></i> S\u1ED1 \u0111i\u1EC7n tho\u1EA1i kh\xF4ng t\u1ED3n t\u1EA1i tr\xEAn h\u1EC7 th\u1ED1ng!</br>");
+                $("#send-mobile").next("span").html('<i class="fa fa-exclamation-circle"></i> S\u1ED1 \u0111i\u1EC7n tho\u1EA1i kh\xF4ng t\u1ED3n t\u1EA1i tr\xEAn h\u1EC7 th\u1ED1ng!</br>');
             }
         });
         return false;
@@ -76,22 +89,22 @@ $(document).ready(function () {
     //*************Load các quận huyện theo địa danh - menu search
     function data_locale(data) {
         $.each(data, function (key, value) {
-            display_locale += "<option>" + value + "</option>";
+            display_locale += '<option>' + value + '</option>';
         });
     }
     function load_locale() {
         var display_locale = "<option>Quận, Huyện</option>";
         if ($(".locale-item").val() == "Hà Nội") {
             $.each(data_json.district_locale.district_hn, function (key, value) {
-                display_locale += "<option>" + value + "</option>";
+                display_locale += '<option>' + value + '</option>';
             });
         } else if ($(".locale-item").val() == "Đà Nẵng") {
             $.each(data_json.district_locale.district_dn, function (key, value) {
-                display_locale += "<option>" + value + "</option>";
+                display_locale += '<option>' + value + '</option>';
             });
         } else if ($(".locale-item").val() == "TP.Hồ Chí Minh") {
             $.each(data_json.district_locale.district_hcm, function (key, value) {
-                display_locale += "<option>" + value + "</option>";
+                display_locale += '<option>' + value + '</option>';
             });
         } else {
             display_locale += "<option>Quận, Huyện</option>";
@@ -105,10 +118,10 @@ $(document).ready(function () {
     // JS load tin cho trang tin tức
 
     var _loop = function _loop(i) {
-        $("#news" + i).click(function () {
+        $('#news' + i).click(function () {
             $.get("news_detail.html", function (data) {
                 $("#data-new").html(data);
-                $("#data-new").html($("#new" + i).html());
+                $("#data-new").html($('#new' + i).html());
             });
         });
     };
@@ -116,4 +129,68 @@ $(document).ready(function () {
     for (var i = 1; i <= 5; i++) {
         _loop(i);
     }
+
+    // JS CHO TRANG MANAGE.HTML - TRANG QUẢN LÝ
+
+
+    /********** Trạng thái bài đăng DEMO CHO 2 BÀI ĐĂNG**********/
+    var date_current = new Date();
+    var msec_date_current = Date.parse(date_current);
+    //demo date 1
+    var date_st1 = new Date("2017-05-19");
+    function date_status(date_st1) {
+        var msec_dateup1 = Date.parse(date_st1);
+        var msec_datedead1 = msec_dateup1 + 5 * 86400000; //Date_dead = date_up + 5days
+        var date_dead1 = new Date(msec_datedead1);
+        $(".date-up.item1").html(date_st1.getDate() + "-" + (date_st1.getMonth() + 1) + "-" + date_st1.getFullYear());
+        $(".date-dead.item1").html(date_dead1.getDate() + "-" + (date_dead1.getMonth() + 1) + "-" + date_dead1.getFullYear());
+        //So sánh ngày hết hạn và thời gian hiện tại
+        if (msec_datedead1 < msec_date_current) {
+            $('#status').html("Tin đã hết hạn");
+            $('#status').css("color", "red");
+        }
+    }
+    //demo date 2
+    var date_st2 = new Date("2017-04-28");
+    function date_status2(date_st2) {
+        var msec_dateup2 = Date.parse(date_st2);
+        var msec_datedead2 = msec_dateup2 + 5 * 86400000; //Date_dead = date_up + 5days
+        var date_dead2 = new Date(msec_datedead2);
+        $(".date-up.item2").html(date_st2.getDate() + "-" + (date_st2.getMonth() + 1) + "-" + date_st2.getFullYear());
+        $(".date-dead.item2").html(date_dead2.getDate() + "-" + (date_dead2.getMonth() + 1) + "-" + date_dead2.getFullYear());
+        //So sánh ngày hết hạn và thời gian hiện tại
+        if (msec_datedead2 < msec_date_current) {
+            $('#status2').html("Tin đã hết hạn");
+        }
+    }
+    window.onload = date_status2(date_st2);
+    window.onload = date_status(date_st1);
+    //Thao tác dừng đăng tin và đăng lại tin
+    $("#st1").click(function () {
+        $('#status').html("Tin đã ngừng đăng");
+        $('#status').css("color", "#ED2327");
+        $(".date-up.item1").html("");
+        $(".date-dead.item1").html("");
+    });
+
+    $("#st2").click(function () {
+        date_st1 = date_current; //cập nhật ngày đăng theo ngày gia hạn
+        date_status(date_st1);
+        $('#status').html("Tin đang được đăng");
+        $('#status').css("color", "#4CAF50");
+    });
+
+    $("#st1-2").click(function () {
+        $('#status2').html("Tin đã ngừng đăng");
+        $('#status2').css("color", "#ED2327");
+        $(".date-up.item2").html("");
+        $(".date-dead.item2").html("");
+    });
+    $("#st2-2").click(function () {
+        date_st2 = date_current; //cập nhật ngày đăng theo ngày gia hạn
+        date_status2(date_st2);
+        $('#status2').html("Tin đang được đăng");
+        $('#status2').css("color", "#4CAF50");
+    });
+    ///////////////////////////////////
 });
--- app/jses6/home.js
+++ app/jses6/home.js
@@ -1,4 +1,17 @@
-//**Scroll fixed menu navigation-mobile,tablet
+/********** Load ảnh đại diện trang Manager  **********/
+function previewFile() {
+	let preview = document.getElementById('displayImg');
+	let file    = document.querySelector('input[type=file]').files[0];
+	let reader  = new FileReader();
+
+	reader.addEventListener("load", function () {
+		preview.src = reader.result;
+	}, false);
+
+	if (file) {
+		reader.readAsDataURL(file);
+	}
+}
 $(document).ready(function() {
     /***Gán dữ liệu vào menu search khi chọn loại hình tim kiem******/
     $(".room").on('click',function(){
@@ -114,5 +127,70 @@ $(document).ready(function() {
             });
         });
     }
+
+    // JS CHO TRANG MANAGE.HTML - TRANG QUẢN LÝ
+
+
+	/********** Trạng thái bài đăng DEMO CHO 2 BÀI ĐĂNG**********/
+	let date_current = new Date();
+	let msec_date_current = Date.parse(date_current);
+  //demo date 1
+	let date_st1 = new Date("2017-05-19");
+	function date_status(date_st1){
+		let msec_dateup1 = Date.parse(date_st1);
+		let msec_datedead1 = msec_dateup1 + (5*86400000);//Date_dead = date_up + 5days
+		let date_dead1 = new Date(msec_datedead1);
+		$(".date-up.item1").html(date_st1.getDate()+"-"+(date_st1.getMonth()+1)+"-"+date_st1.getFullYear());
+		$(".date-dead.item1").html(date_dead1.getDate()+"-"+(date_dead1.getMonth()+1)+"-"+date_dead1.getFullYear());
+		//So sánh ngày hết hạn và thời gian hiện tại
+		if (msec_datedead1 < msec_date_current) {
+			$('#status').html("Tin đã hết hạn");
+			$('#status').css("color","red");
+		}
+	}
+  //demo date 2
+	let date_st2 = new Date("2017-04-28");
+	function date_status2(date_st2){
+		let msec_dateup2 = Date.parse(date_st2);
+		let msec_datedead2 = msec_dateup2 + (5*86400000);//Date_dead = date_up + 5days
+		let date_dead2 = new Date(msec_datedead2);
+		$(".date-up.item2").html(date_st2.getDate()+"-"+(date_st2.getMonth()+1)+"-"+date_st2.getFullYear());
+		$(".date-dead.item2").html(date_dead2.getDate()+"-"+(date_dead2.getMonth()+1)+"-"+date_dead2.getFullYear())
+		//So sánh ngày hết hạn và thời gian hiện tại
+		if (msec_datedead2 < msec_date_current) {
+			$('#status2').html("Tin đã hết hạn")
+		}
+	}
+	window.onload = date_status2(date_st2);
+	window.onload = date_status(date_st1)
+//Thao tác dừng đăng tin và đăng lại tin
+	$("#st1").click(function(){
+		$('#status').html("Tin đã ngừng đăng");
+		$('#status').css("color","#ED2327");
+		$(".date-up.item1").html("");
+		$(".date-dead.item1").html("")
+	});
+
+	$("#st2").click(function(){
+		date_st1 = date_current;//cập nhật ngày đăng theo ngày gia hạn
+		date_status(date_st1);
+		$('#status').html("Tin đang được đăng");
+		$('#status').css("color","#4CAF50");
+	});
+
+	$("#st1-2").click(function(){
+		$('#status2').html("Tin đã ngừng đăng");
+		$('#status2').css("color","#ED2327");
+		$(".date-up.item2").html("");
+		$(".date-dead.item2").html("")
+	});
+	$("#st2-2").click(function(){
+		date_st2 = date_current;//cập nhật ngày đăng theo ngày gia hạn
+		date_status2(date_st2);
+		$('#status2').html("Tin đang được đăng");
+		$('#status2').css("color","#4CAF50");
+	})
+	///////////////////////////////////
+
 });
 
